811cfb35650d97f85915bc7e0b74964288150cfb
Jacken Server <wyg132456@163.com>
2025-04-01 22:52:22 +0800

    .

--- generate_conversation.py
+++ generate_conversation.py
@@ -4,5 +4,5 @@ from erling.chat import generate_vec, generate_conversation
 
 # 放你自己的路径
 data_path = "/home/jacken/files/Erling_test/data/"
-generate_vec(data_path)
+# generate_vec(data_path)
 generate_conversation(data_path)
